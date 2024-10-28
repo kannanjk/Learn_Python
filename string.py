ec3b14927c88ff82cdb6c8f9fdbbf9aed5085539
kannanjk <jishnukumbadam@gmail.com>
2024-10-28 12:08:24 +0530

    string

--- string.py
+++ string.py
@@ -1,26 +1,26 @@
-# a = "Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididuntut labore et dolore magna aliqua."
-# print(a)
+a = "Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididuntut labore et dolore magna aliqua."
+print(a)
 
-# a = "Hello, World!"
-# print(len(a))
+a = "Hello, World!"
+print(len(a))
 
-# for x in "banana":
-#     print(x)
+for x in "banana":
+    print(x)
 
-# txt = "The best things in life are free!"
-# print(txt [:8])
-# if "jishnu" in txt :
-#     print("yes true")
-# else:
-#     print("no false")
+txt = "The best things in life are free!"
+print(txt [:8])
+if "jishnu" in txt :
+    print("yes true")
+else:
+    print("no false")
 
-# b=34/
-# a = f"Hello {b*5} World!",4
-# print(a.upper())
+b=34
+a = f"Hello {b*5} World!",4
+print(a.upper())
 
-# print(a.strip())
+print(a.strip())
 
-# print(a.replace("H", "J"))
+print(a.replace("H", "J"))
 
 a = "thomaskutti \'vittoda...\' helo world "
 
