cbbad78322108ab8e8d119da98b3514dbd94fb74
kannanjk <jishnukumbadam@gmail.com>
2024-10-28 12:07:41 +0530

    string

--- string.py
+++ string.py
@@ -1,2 +1,27 @@
-a = "Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididuntut labore et dolore magna aliqua."
+# a = "Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididuntut labore et dolore magna aliqua."
+# print(a)
+
+# a = "Hello, World!"
+# print(len(a))
+
+# for x in "banana":
+#     print(x)
+
+# txt = "The best things in life are free!"
+# print(txt [:8])
+# if "jishnu" in txt :
+#     print("yes true")
+# else:
+#     print("no false")
+
+# b=34/
+# a = f"Hello {b*5} World!",4
+# print(a.upper())
+
+# print(a.strip())
+
+# print(a.replace("H", "J"))
+
+a = "thomaskutti \'vittoda...\' helo world "
+
 print(a)
