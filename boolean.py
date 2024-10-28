55ff301989f7818e83edd75380f5f58e7fc8ff42
kannanjk <jishnukumbadam@gmail.com>
2024-10-28 12:15:09 +0530

    bool

---
+++ boolean.py
@@ -0,0 +1,22 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+print(bool("kannan"))
+print(bool('s'))
+
+def myFunction() :
+  return False
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
