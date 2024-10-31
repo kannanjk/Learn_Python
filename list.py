ab07db60c6de12209f7bb39fefc022cdb4cbc6e5
kannanjk <jishnukumbadam@gmail.com>
2024-10-31 14:53:59 +0530

    basic

---
+++ list.py
@@ -0,0 +1,11 @@
+# thislist = ["apple", "banana", "cherry"]
+# for d in thislist:
+#   print(d)
+
+# thislist = ["apple", "banana", "cherry"]
+# for i in range(len(thislist)):
+#   print(len(thislist))
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newList = [x for x in fruits]
